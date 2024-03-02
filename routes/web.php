b5b79bee1b854d0ed2f9b3ec7284db2efff3ec5d
Richelle <arat.richellemae@gmail.com>
2024-03-02 23:13:07 +0800

    final

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->get('/users',['uses' => 'UserController@getUsers']);
+$router->post('/users',['uses' => 'UserController@add']);
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
